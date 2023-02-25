9bc2813f5bca36060dd4699b485f1f944bd52f52
maqiie <99504010+maqiie@users.noreply.github.com>
2023-02-26 00:18:08 +0300

    completed the lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'Brown', 'Cuddly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 13, 'M', 'Black', 'Malicious', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 8, 'F', 'Brown', 'Joyful', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 43, 'F', 'Black', 'Devoted', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 6, 'F', 'Black', 'Playful', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 2, 'M', 'White', 'Social', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 7, 'F', 'Brown', 'Altruistic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 5, 'F', 'Black', 'Sensitive', 1);
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,19 +1,29 @@
+# selects all of the female bears and returns their name and age
 def selects_all_female_bears_return_name_and_age
   "Write your SQL query here"
+  "SELECT name, age FROM bears WHERE sex = 'F';"
 end
 
+# selects all of the bears names and orders them in alphabetical order
 def selects_all_bears_names_and_orders_in_alphabetical_order
   "Write your SQL query here"
+  "SELECT name FROM bears ORDER BY name ASC;"
 end
 
+# selects all of the bears names and ages that are alive and order them from youngest to oldest
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
   "Write your SQL query here"
+  "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age ASC;"
 end
 
+# selects the oldest bear and returns their name and age
 def selects_oldest_bear_and_returns_name_and_age
   "Write your SQL query here"
+  "SELECT name, age FROM bears WHERE (age = (SELECT MAX(age) FROM bears)) AND (name<>'null') "
 end
 
+# selects the youngest bear and returns their name and age
 def select_youngest_bear_and_returns_name_and_age
   "Write your SQL query here"
+  "SELECT name, age FROM bears WHERE age = (SELECT MIN(age) FROM bears WHERE age IS NOT NULL);"
 end
